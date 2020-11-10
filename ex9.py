617376891a2441ce23716c854da0417524dbec17
Zachary Martinez <martinez.zach.m@gmail.com>
2020-11-09 22:06:27 -0800

    Complete exercise 9

---
+++ ex9.py
@@ -0,0 +1,20 @@
+# Declare a string with a list of the abbreviated names of the days of the week
+# seperated by a space.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Declare a string with abbreviated names of the months January-August,
+# seperated by a newline character.
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print a string, followed by the variable days.
+print("Here are the days: ", days)
+
+# Print a string, followed by the variable months.
+print("Here are the months: ", months)
+
+# Print a multiline string.
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
