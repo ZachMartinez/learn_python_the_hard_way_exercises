1c67f69ef26ec0298ce3c9efbc49db2254dc11df
Zachary Martinez <martinez.zach.m@gmail.com>
2020-11-09 21:37:28 -0800

    Complete exercise 7

---
+++ ex7.py
@@ -0,0 +1,38 @@
+#Print strind to terminal
+print("Mary had a little lamb.")
+# Print string to terminal with space for format. Put string 'snow'
+# in format string
+print("It's fleece was white as {}.".format('snow'))
+# Print string to terminal
+print("And everywhere that Mary went.")
+print("." * 10) # what'd that do? It will print ".........."
+# Declare variable end1 with value "C"
+end1 = "C"
+# Declare variable end2 with variable "h"
+end2 = "h"
+# Declare variable end3 with value "e"
+end3 = "e"
+# Declare variable end4 with value "e"
+end4 = "e"
+# Declare variable end5 with value "s"
+end5 = "s"
+# Declare variable end6 with value "e"
+end6 = "e"
+# Declare variable end7 with value "B"
+end7 = "B"
+# Declare variable end8 with value "u"
+end8 = "u"
+# Declare variable end9 with value "r"
+end9 = "r"
+# Declare variable end10 with value "g"
+end10 = "g"
+# Declare variable end11 with value "e"
+end11 = "e"
+# Declare variable end12 with value "r"
+end12 = "r"
+
+# watch end = '' at the end. try removing it to see what happens
+# Print string end1 - end6 on one line and pad with ' '.
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# Print strings end7 - end12 on one line.
+print(end7 + end8 + end9 + end10 + end11 + end12)
